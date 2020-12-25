d2c1de52a4e1d9e1c8bfa2461d18f8316669bbbf
Torkel <torkel@voysys.se>
2020-12-25 23:33:05 +0900

    day 19 (almost, at least)

--- day_19_b/src/main.rs
+++ day_19_b/src/main.rs
@@ -1,149 +1,140 @@
-use std::collections::HashMap;
+// This solution is not mine!
+// I didn't have the energy to figure out what went wrong on one of the parsing cases the way I did things...
+// So I borrowed from this solution:
+// https://github.com/prscoelho/aoc2020/blob/main/src/day19/day19.rs
+
+use std::collections::{HashMap, VecDeque};
 use std::error::Error;
 use std::fs;
 
-#[derive(Clone, Debug, PartialEq)]
-enum Character {
-    A,
-    B,
+#[derive(Debug, Clone)]
+enum Rule {
+    Literal(char),        // literal
+    Standard(Vec<usize>), // list of rules which rule matches
 }
 
-#[derive(Clone, Debug, PartialEq)]
-enum Token {
-    Number { n: i32 },
-    Character { c: Character },
+fn parse_rule_part(text: &str) -> Rule {
+    Rule::Standard(
+        text.split_ascii_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect::<Vec<usize>>(),
+    )
 }
 
-fn match_rules(
-    message: &[char],
-    message_i: usize,
-    rule_i: i32,
-    rules: &HashMap<i32, Vec<Vec<Token>>>,
-) -> (bool, usize) {
-    if message_i >= message.len() {
-        return (true, 0);
+fn parse_rule(text: &str) -> Vec<Rule> {
+    let mut result = Vec::new();
+    if text.contains("\"") {
+        result.push(Rule::Literal(text.chars().skip(1).next().unwrap()));
+    } else if text.contains("|") {
+        for part in text.split(" | ") {
+            result.push(parse_rule_part(part));
+        }
+    } else {
+        result.push(parse_rule_part(text));
+    }
+    result
+}
+
+fn parse_rules(rules_str: &str) -> HashMap<usize, Vec<Rule>> {
+    let mut rules = HashMap::new();
+
+    for line in rules_str.lines() {
+        let mut it = line.split(": ");
+        let left_str = it.next().unwrap();
+        let right_str = it.next().unwrap();
+
+        assert_eq!(it.next(), None);
+
+        let left = left_str.parse().unwrap();
+        let right = parse_rule(right_str);
+
+        rules.insert(left, right);
     }
 
-    let mc = match message[message_i] {
-        'a' => Character::A,
-        'b' => Character::B,
-        _ => panic!(),
-    };
+    rules
+}
 
-    match rules[&rule_i].first().unwrap().first().unwrap() {
-        Token::Character { c } => {
-            if &mc == c {
-                return (true, 1);
-            } else {
-                return (false, 0);
-            }
-        }
+fn matches(rules: &HashMap<usize, Vec<Rule>>, phrase: &[char], mut with: VecDeque<usize>) -> bool {
+    match (phrase.len(), with.len()) {
+        (0, 0) => return true,  // matches is true only if phrase and with is empty
+        (_, 0) => return false, // it can't match if phrase is empty and with is not, likewise for the reverse
+        (0, _) => return false,
         _ => {}
     }
 
-    for tokens in &rules[&rule_i] {
-        let mut is_ok = true;
-        let mut tmp_message_i = message_i;
-        for token in tokens {
-            match token {
-                Token::Number { n } => {
-                    let (next_res_ok, consumed) = match_rules(message, tmp_message_i, *n, rules);
-                    if !next_res_ok {
-                        is_ok = false;
-                        break;
-                    }
-                    tmp_message_i += consumed;
+    let rule_to_expand = with.pop_front().unwrap();
+    let possibilities = &rules[&rule_to_expand];
+    for rule in possibilities {
+        let result = match rule {
+            Rule::Literal(c) => {
+                if c == &phrase[0] {
+                    // character matched with next rule literal, call matches with one less character and rest of with.
+                    let next_with = with.clone();
+                    matches(&rules, &phrase[1..], next_with)
+                } else {
+                    // character didn't match with the expanded rule
+                    false
                 }
-                _ => panic!("{:?}", token),
             }
-        }
-        if is_ok {
-            println!("rule {} ({:?})", rule_i, tokens);
-            return (true, tmp_message_i - message_i);
+            Rule::Standard(expanded) => {
+                // we werent able to match with a character, expand the popped rule and try to match [expanded, rest]
+                let next_with: VecDeque<usize> =
+                    expanded.iter().chain(with.iter()).copied().collect();
+                if expanded.len() > phrase.len() {
+                    // if the total expanded rule size is bigger than phrase size, it can't possibly match.
+                    // there are no empty rules, each rule will match at least one character
+                    false
+                } else {
+                    matches(&rules, &phrase, next_with)
+                }
+            }
+        };
+        if result {
+            return true;
         }
     }
+    false
+}
 
-    (false, 0)
+fn modify_part2(rules: &mut HashMap<usize, Vec<Rule>>) {
+    let rule8 = vec![Rule::Standard(vec![42]), Rule::Standard(vec![42, 8])];
+    let rule11 = vec![
+        Rule::Standard(vec![42, 31]),
+        Rule::Standard(vec![42, 11, 31]),
+    ];
+    if let Some(rule) = rules.get_mut(&8) {
+        *rule = rule8;
+    }
+    if let Some(rule) = rules.get_mut(&11) {
+        *rule = rule11;
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = fs::read_to_string("input_test")?;
+    let input = fs::read_to_string("input")?;
 
-    let main_parts: Vec<&str> = input.split("\n\n").collect();
-    let rules_raw: &str = main_parts[0];
-    let messages: &str = main_parts[1];
+    let mut it = input.split("\n\n");
+    let mut rules = parse_rules(it.next().unwrap());
 
-    let mut rules: HashMap<i32, Vec<Vec<Token>>> = HashMap::new();
+    modify_part2(&mut rules);
 
-    for rule_line in rules_raw.lines() {
-        let rule_line_parts: Vec<&str> = rule_line.split(": ").collect();
-        let n = rule_line_parts[0].parse::<i32>().unwrap();
-        let mut outputs: Vec<Vec<Token>> = Vec::new();
-        for outputs_str in rule_line_parts[1].split("|") {
-            let mut output: Vec<Token> = Vec::new();
-            //println!("outputs_str: {:?}", outputs_str);
-            for o in outputs_str
-                .split(" ")
-                .map(|s| s.trim())
-                .filter(|s| !s.is_empty())
-            {
-                match o.chars().nth(0).unwrap() {
-                    '1'..='9' => {
-                        output.push(Token::Number {
-                            n: o.parse::<i32>().unwrap(),
-                        });
-                    }
-                    '"' => match o {
-                        "\"a\"" => {
-                            output.push(Token::Character { c: Character::A });
-                        }
-                        "\"b\"" => {
-                            output.push(Token::Character { c: Character::B });
-                        }
-                        _ => panic!(),
-                    },
-                    _ => panic!(),
-                }
-            }
-            outputs.push(output);
-        }
-        rules.insert(n, outputs);
-    }
+    let strings: Vec<Vec<char>> = it
+        .next()
+        .unwrap()
+        .lines()
+        .map(|s| s.chars().collect())
+        .collect();
 
-    //8: 42 | 42 8
-    //11: 42 31 | 42 11 31
+    let mut start = VecDeque::new();
+    start.push_back(0);
 
-    rules.insert(
-        8,
-        vec![
-            vec![Token::Number { n: 42 }],
-            vec![Token::Number { n: 42 }, Token::Number { n: 8 }],
-        ],
-    );
-    rules.insert(
-        11,
-        vec![
-            vec![Token::Number { n: 42 }, Token::Number { n: 31 }],
-            vec![
-                Token::Number { n: 42 },
-                Token::Number { n: 11 },
-                Token::Number { n: 31 },
-            ],
-        ],
-    );
+    let res = strings
+        .into_iter()
+        .map(|s| matches(&rules, &s, start.clone()))
+        .filter(|&b| b)
+        .count();
 
-    let mut res = 0;
-
-    for message in messages.lines() {
-        let tmp_chars: Vec<char> = message.chars().collect();
-        let (match_res, match_len) = match_rules(&tmp_chars, 0, 0, &rules);
-        if match_res && match_len == message.len() {
-            println!("ok: {}", message);
-            res += 1;
-        }
-    }
-
-    println!("res: {}", res);
+    println!("{:?}", res);
 
     Ok(())
 }
